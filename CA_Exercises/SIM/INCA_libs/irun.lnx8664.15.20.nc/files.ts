d2225b4a7a1af57ca82e8b8dc1003f61306d8a50
avaodwyer <avaodwyer3@gmail.com>
2023-04-28 12:18:03 +0200

    Mutl2 works fine

--- CA_Exercises/SIM/INCA_libs/irun.lnx8664.15.20.nc/files.ts
+++ CA_Exercises/SIM/INCA_libs/irun.lnx8664.15.20.nc/files.ts
@@ -1,8 +1,8 @@
 1682672165 /users/students/r0916050/Documents/CA_Exercise/CA_Exercises/Verilog/RTL/alu.v
-1682674678 /users/students/r0916050/Documents/CA_Exercise/CA_Exercises/Verilog/RTL/alu_control.v
+1682676987 /users/students/r0916050/Documents/CA_Exercise/CA_Exercises/Verilog/RTL/alu_control.v
 1681979535 /users/students/r0916050/Documents/CA_Exercise/CA_Exercises/Verilog/RTL/branch_unit.v
 1682674620 /users/students/r0916050/Documents/CA_Exercise/CA_Exercises/Verilog/RTL/control_unit.v
-1681979535 /users/students/r0916050/Documents/CA_Exercise/CA_Exercises/Verilog/RTL/cpu.v
+1682677031 /users/students/r0916050/Documents/CA_Exercise/CA_Exercises/Verilog/RTL/cpu.v
 1681979535 /users/students/r0916050/Documents/CA_Exercise/CA_Exercises/Verilog/RTL/immediate_extend_unit.v
 1681979535 /users/students/r0916050/Documents/CA_Exercise/CA_Exercises/Verilog/RTL/mux_2.v
 1681979535 /users/students/r0916050/Documents/CA_Exercise/CA_Exercises/Verilog/RTL/pc.v
